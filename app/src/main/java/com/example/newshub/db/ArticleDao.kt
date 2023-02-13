5ef77621c56e69ec92359fd33d2c00a59ab3245d
rohan_anand <rohan.choudhary@indiamart.com>
2023-02-13 19:47:33 +0530

    setup dao and room db for article

---
+++ app/src/main/java/com/example/newshub/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.example.newshub.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newshub.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateOrInsert(article: Article):Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
